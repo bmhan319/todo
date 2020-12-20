8b119833173e2ca9ed65c4f772d96f20db0dd4b8
Brian Han <brianhan@brians-mbp.lan>
2020-12-20 12:25:45 -0800

    setup keyboard accessibility to the app

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react'
 
 export default class Todo extends Component {
+  //Creating keyboard functionality to run the filter sort function
+  onKeyPress = (e) => {
+    const status = e.target.dataset.filter
+    if (e.charCode === 13){
+      this.props.filter(status)
+    }
+  }
 
   //Upon deleting all list items, Empty list element is displayed
   reset = () => {
@@ -24,7 +31,7 @@ export default class Todo extends Component {
           </div>
 
           <form className={`listInputContainer inputError inputError${this.props.state.inputError} listBg${this.props.state.mode}`} onSubmit={ this.props.handleSubmit }>
-            <button className={`listCheckDesign listDesign${this.props.state.mode} listSubmit`} type="submit"  >
+            <button tabIndex="-1" className={`listCheckDesign listDesign${this.props.state.mode} listSubmit`} type="submit"  >
               <div className={`innerButton innerButton${this.props.state.mode}`}>
                 <svg className="listSubmitCheck" xmlns="http://www.w3.org/2000/svg" width="11" height="9">
                   <path className={`strokeNotDone listSubmitPath`} fill="none" stroke="#FFF" strokeWidth="2" d="M1 4.304L3.696 7l6-6"/>
@@ -41,7 +48,7 @@ export default class Todo extends Component {
             </li>
             { this.props.state.list.map( (item,ind) => (
               <li key={ind + item.todo} className={`listItem listBorder${this.props.state.mode} listItem${ind}`} data-status={item.status} style={{display: item.display}} >
-                <button className={`listCheckDesign listDesign${this.props.state.mode} listItemComplete checkBg${item.status} listComplete${ind}`} type="button" onClick={ ()=>{this.props.complete(ind)} } >
+                <button tabIndex="0" className={`listTab listCheckDesign listDesign${this.props.state.mode} listItemComplete checkBg${item.status} listComplete${ind}`} type="button" onClick={ ()=>{this.props.complete(ind)} } >
                   <div className={`innerButton innerButton${ind} innerButton${this.props.state.mode} button${item.status}`} >
                     <svg className="listItemCheck" xmlns="http://www.w3.org/2000/svg" width="11" height="9">
                       <path className={`stroke${item.status} listItemPath listItemPath${ind}`} fill="none" stroke="#FFF" strokeWidth="2" d="M1 4.304L3.696 7l6-6"/>
@@ -49,18 +56,18 @@ export default class Todo extends Component {
                   </div>
                 </button>
                 <p className={`strike${item.status}ListText${this.props.state.mode} listItemSubject listItemSubject${ind} listText${this.props.state.mode}`} >{item.todo}</p>
-                <div className="closeIcon" onClick={ ()=>{this.props.close(ind)} } ></div>
+                <div tabIndex="0" className="closeIcon" onKeyPress={ ()=>{this.props.close(ind)} } onClick={ ()=>{this.props.close(ind)} } ></div>
               </li>
             ) ) }
             
             <li className="listTailItem">
               <p className={`tailItemText tailItemText${this.props.state.mode} itemsLeft`}>{this.props.state.list.length} items left</p>
-              <p  onClick={ ()=>{this.props.filter('Clear')} } className={`tailItemText tailItemText${this.props.state.mode} completedItems completedItems${this.props.state.mode}`}>Clear Completed</p>
+              <p tabIndex="0" onKeyPress={this.onKeyPress}  onClick={ ()=>{this.props.filter('Clear')} } className={`tailItemText tailItemText${this.props.state.mode} completedItems completedItems${this.props.state.mode}`} data-filter="Clear">Clear Completed</p>
               <div className={`sortContainer listBg${this.props.state.mode}`}>
                 <div className="sortTextContainer">
-                  <p onClick={ ()=>{this.props.filter('All')} } className={`sortText tailText tailText${this.props.state.mode} sortClear sortAll`}>All</p>
-                  <p onClick={ ()=>{this.props.filter('Active')} } className={`sortText tailText tailText${this.props.state.mode} sortActive`}>Active</p>
-                  <p onClick={ ()=>{this.props.filter('Complete')} } className={`sortText tailText tailText${this.props.state.mode} sortComplete`}>Completed</p>
+                  <p tabIndex="0" onKeyPress={this.onKeyPress} onClick={ ()=>{this.props.filter('All')} } className={`sortText tailText tailText${this.props.state.mode} sortClear sortAll`} data-filter="All" >All</p>
+                  <p tabIndex="0" onKeyPress={this.onKeyPress} onClick={ ()=>{this.props.filter('Active')} } className={`sortText tailText tailText${this.props.state.mode} sortActive`} data-filter="Active" >Active</p>
+                  <p tabIndex="0" onKeyPress={this.onKeyPress} onClick={ ()=>{this.props.filter('Complete')} } className={`sortText tailText tailText${this.props.state.mode} sortComplete`} data-filter="Complete" >Completed</p>
                 </div> 
               </div>
             </li>
